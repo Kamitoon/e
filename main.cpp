0bffd79d45e959401ced82a2cac75443a7db6b7f
kami <57278362+Kamitoon@users.noreply.github.com>
2020-05-30 15:41:10 -0300

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+struct banco{
+    
+};
+int main(){
+    banco op;
+    do{
+        switch(op){
+        case : 
+        }
+    } while (op!=0);
+    
+    return 0;
+}
